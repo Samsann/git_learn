a85f5d4a16b4a2d4440248e2aa373b2ee51ae8b4
Sahil Ninawe <sahilninawe302@gmail.com>
2024-05-30 00:49:22 +0530

    Commit for Multiply.cpp in (sahil/Multiply) of dev branch in master

---
+++ multiply.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,Answer;
+    cout<<"Enter the values for a and b"<<endl;
+    cin>>a;
+    cin>>b;
+    cout<<"The multiplication for a and b is "<<a*b;
+    return a*b;
+}
